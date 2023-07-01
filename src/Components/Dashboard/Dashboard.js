c7ea9ba56c35ee04324e4b28aecb8cdf76a14a9c
unknown <shilpa.keswai28111994@gmail.com>
2023-07-01 11:53:28 +0530

    Added folder structures

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,18 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Dashboard.css';
 // import File from '../File/File';
+import {FcFolder} from 'react-icons/fc';
 import Folder from '../Folder/Folder';
-// import axios from  'axios';
+import axios from 'axios';
 function Dashboard() {
+
+  const [folders, setFolder] = useState([]);
+  const getFolderContentController=async(id)=>{
+    console.log('Id of Folder: ',id);
+    try {
+      const folder = await axios.get(`http://localhost:8080/${id}`);
+
+      console.log('Folder__Clicked: ',folder);
+    }
+    catch (err) {
+      console.log('Error : ', err);
+    }
+  }
+  const getAllFolder = async () => {
+    try {
+      const folder = await axios.get('http://localhost:8080/');
+      setFolder(folder.data);
+      
+    }
+    catch (err) {
+      console.log('Error : ', err);
+    }
+  }
+  useEffect(() => {
+    getAllFolder();
+  }, [])
+  console.log('Folders: ', folders);
   return (
     <div>
-        <div className='header'>
-            <h1 className='heading'>Data Management System</h1>
-        </div>
-        {/* <File/> */}
-        <Folder/>
+      <div className='header'>
+        <h1 className='heading'>Data Management System</h1>
+      </div>
+      {/* <File/> */}
+      <Folder/>
+      <div className='container'>
+      <div className="row">
+      {
+        folders.map((folder) => {
+          return (
+            <div className="col-sm-2" key={folder._id}>
+            <div className="card">
+              
+              <div className="card-body"><FcFolder size={30} style={{cursor:'pointer'} } onClick={()=>{getFolderContentController(folder._id)}}/><br/>
+                <h5 className="card-title">{folder.folderName}</h5>
+                <p className="card-text">Creation Date</p>
+              </div>
+            </div>
+            </div>
+          )
+        })
+      }
+      </div></div>
     </div>
-    
+
   )
 }
 
--- src/Components/Folder/Folder.js
+++ src/Components/Folder/Folder.js
@@ -2,15 +2,19 @@ import React,{useState} from 'react'
 import axios from 'axios';
 function Folder() {
     const [folder,setFolder]=useState('');
-    const createFolder=async(folder)=>{
+    const createFolder=async()=>{
+      try {
         console.log('Folder: ',folder);
         const response=await axios.post('http://localhost:8080/createFolder',{'folderName':folder});
         console.log('Response added: ',response);
+      } catch (error) {
+        console.log('At the time of creating folder: ',error) 
+      }
     }
   return (
     <div>
         <input type='text'  placeholder='Enter folder name :' onChange={(e)=>{setFolder(e.target.value)}}/>
-        <button type='submit' onClick={()=>{createFolder(folder)}}>Create</button>
+        <button type='submit' onClick={()=>{createFolder()}}>Create</button>
     </div>
   )
 }
